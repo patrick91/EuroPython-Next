369cef901cfb28c9575a70b915a8a8a0844a94a2
Christian Barra <barrachri@gmail.com>
2015-08-03 23:16:34 +0200

    added django-rest-framework, user and conference app

--- Backend/europython/settings/base.py
+++ Backend/europython/settings/base.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'user',
+    'conference'
 )
 
 MIDDLEWARE_CLASSES = (
@@ -79,6 +82,12 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
+}
+
+# Rest framework
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.IsAdminUser',),
+    'PAGE_SIZE': 10
 }
 
 
