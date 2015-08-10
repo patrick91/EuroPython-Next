4e8ca72f778284ec4bf7bd20fde684c6a31e1bdf
Christian Barra <barrachri@gmail.com>
2015-08-10 10:49:02 +0200

    added token auth

--- Backend/europython/settings/base.py
+++ Backend/europython/settings/base.py
@@ -92,10 +92,9 @@ DATABASES = {
 # Rest framework
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.IsAdminUser',),
-    'PAGE_SIZE': 10
+    'PAGE_SIZE': 10,
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.BasicAuthentication',
-        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.TokenAuthentication'
     )
 }
 
